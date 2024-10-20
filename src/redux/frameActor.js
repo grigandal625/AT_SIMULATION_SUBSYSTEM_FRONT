0a38e046da0e41c6c912ffa21cd78d445e68b916
grigandal625 <grigandal625@gmail.com>
2024-10-21 00:42:27 +0300

    fix

--- src/redux/frameActor.js
+++ src/redux/frameActor.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 export const createFrameActionAsyncThunk = (type, payloadCreator, options) => {
     const newPayloadCreator = async (...args) => {
-        result = await payloadCreator(...args);
+        const result = await payloadCreator(...args);
         if (window.sessionStorage.getItem("frameId")) {
             window.parent.postMessage({ type: "action", event: type, data: { args: args[0], result }, frameId: window.sessionStorage.getItem("frameId") }, "*");
         }
